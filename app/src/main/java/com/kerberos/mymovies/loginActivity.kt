1d6064fc90ec6dda0c7caec81895f4a978be2a00
kerberos <whizydan@gmail.com>
2022-10-08 16:00:34 +0300

    Initial commit

---
+++ app/src/main/java/com/kerberos/mymovies/loginActivity.kt
@@ -0,0 +1,11 @@
+package com.kerberos.mymovies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class loginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
